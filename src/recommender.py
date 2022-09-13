9993b5e0a460f4fbfa76558e77cf5b6246702e69
Allen Wang <allenwang52@yahoo.com>
2022-09-12 22:14:48 -0500

    Updated recommender.py

--- src/recommender.py
+++ src/recommender.py
@@ -51,3 +51,27 @@ if __name__ == '__main__':
     # Checking to see we have no more null values
     #print(anime_df.isnull().sum())
     # No more null values!
+
+    # Convert any -1 rating values to Nan
+    rating_df['rating'] = rating_df['rating'].apply(lambda x: np.nan if x==-1 else x)
+
+    # Get animes that are of TV type
+    anime_df = anime_df[anime_df['type'] == 'TV']
+
+    # Combine anime_df and rating_df
+    rated_anime = rating_df.merge(anime_df, left_on='anime_id', right_on='anime_id', suffixes=['_user', ''])
+    # This is equivalent
+    #rated_anime_inner = rating_df.merge(anime_df, how='inner', on='anime_id', suffixes=['_user', ''])
+
+    # Extract only user_id, name, and rating from the combined df
+    rated_anime = rated_anime[['user_id', 'name', 'rating_user']]
+
+    # Create pivot table to simplify the formatting of the data
+    pivot = rated_anime.pivot_table(index='user_id', columns='name', values='rating_user')
+
+    # Normalize the data because cosine similarity function needs the data to be normalized (-1 to 1 scale)
+    pivot_norm = pivot.apply(lambda x: (x-np.mean(x))/(np.max(x) - np.min(x)), axis=1)
+
+    # Replace NaN values with 0
+    pivot_norm.fillna(0, inplace=True)
+
