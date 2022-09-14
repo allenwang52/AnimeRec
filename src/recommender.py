a156aef916d883e26944cadb3fd3950b2fd22860
spartand4nny <68714119+spartand4nny@users.noreply.github.com>
2022-09-13 19:53:52 -0500

    Update recommender.py

--- src/recommender.py
+++ src/recommender.py
@@ -17,6 +17,7 @@ warnings.filterwarnings("always")
 warnings.filterwarnings("ignore")
 
 if __name__ == '__main__':
+    anime=(input('What show did you recently finish?'))
     # list all files under the input directory
     for dirname, _, filenames in os.walk('data/'):
         for filename in filenames:
@@ -38,32 +39,36 @@ if __name__ == '__main__':
 
     # Percentage of null values in rating_df
     #print(round(rating_df.isnull().sum().sort_values(ascending=False)/len(rating_df.index), 4)*100) 
-    # No null values!
+    # No null values
 
     # Dropping data that has nan values for rating
     anime_df = anime_df[~np.isnan(anime_df["rating"])]
-
+    
     # Filling nan values for genre with "Unknown"
     anime_df["genre"] = anime_df["genre"].fillna("Unknown")
     # Filling nan values for type with mode value of type
     anime_df["type"] = anime_df["type"].fillna(anime_df["type"].dropna().mode().values[0])
-
+    #genre mode here is TV
+    
     # Checking to see we have no more null values
-    #print(anime_df.isnull().sum())
+    #print(anime_df.isnull().sum()); this command prints a sum of all of the cells with null values
     # No more null values!
 
-    # Convert any -1 rating values to Nan
+    # Convert any -1 rating values to Nan; this will be for the normaliing part later (part of the cosine sim model)
     rating_df['rating'] = rating_df['rating'].apply(lambda x: np.nan if x==-1 else x)
+
 
     # Get animes that are of TV type
     anime_df = anime_df[anime_df['type'] == 'TV']
 
-    # Combine anime_df and rating_df
+     # Combine anime_df and rating_df (think of this as a SQL inner join)
     rated_anime = rating_df.merge(anime_df, left_on='anime_id', right_on='anime_id', suffixes=['_user', ''])
     # This is equivalent
     #rated_anime_inner = rating_df.merge(anime_df, how='inner', on='anime_id', suffixes=['_user', ''])
+    #print(rated_anime_inner.shape)
+    #print(rated_anime.shape) way to check total number or rows kept after each join type
 
-    # Extract only user_id, name, and rating from the combined df
+# Extract only user_id, name, and rating from the combined df
     rated_anime = rated_anime[['user_id', 'name', 'rating_user']]
 
     # Create pivot table to simplify the formatting of the data
@@ -74,4 +79,30 @@ if __name__ == '__main__':
 
     # Replace NaN values with 0
     pivot_norm.fillna(0, inplace=True)
+    
+    #now, transpose the data
+    pivot_norm = pivot_norm.T
 
+    #drop values containing 0
+    pivot_norm = pivot_norm.loc[:, (pivot_norm != 0).any(axis = 0)]
+
+    #now, take the new cleaned up table and convert it into a sparsed matrix format for the simularity calculation
+    #sparing = spreading out the data with mostly 0s for the machine learning computation
+    piv_sparse = sp.sparse.csr_matrix(pivot_norm.values)
+    #print('got it') just to make sure everything ran correctly
+
+    #cosine model; this is where the fun begins :)
+    anime_simularity = cosine_similarity(piv_sparse)
+    anime_sim_df = pd.DataFrame(anime_simularity, index = pivot_norm.index, columns = pivot_norm.index)
+    
+   
+    
+    def anime_recommendation(anime):
+        iter = 1
+        print('Since you watched {}, you might like these shows:\n'.format(anime))
+        for i in anime_sim_df.sort_values(by = anime, ascending = False).index[1:6]:
+            print(f'#{iter}: {i}, {round(anime_sim_df[i][anime]*100,2)}% match') 
+            iter += 1
+    
+    anime_recommendation(anime)
+    #print('got it')
